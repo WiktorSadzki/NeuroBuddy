78632116b52038f6137ad788fdedfceff98d3e96
WiktorSadzki <sadzkiwiktor@gmail.com>
2023-10-17 20:31:41 +0200

    Neuro Buddy

---
+++ app/src/main/java/com/example/neurobuddy/JsoupParse.java
@@ -0,0 +1,12 @@
+package com.example.neurobuddy;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+public class JsoupParse {
+    String Grade;
+}
+
